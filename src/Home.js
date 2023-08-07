d9bc8e3ddb453879579f08814ae761f7b8ef85b3
ronanfilloy <ronanfilloy@gmail.com>
2023-08-07 17:44:15 -0300

    changed homepage animation

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { ThemeContext } from './contexts/ThemeContext';
 import { useNavigate } from 'react-router-dom';
-import {CSSTransition} from 'react-transition-group';
+import { CSSTransition } from 'react-transition-group';
 import MyImg from './imgs/FotoPresentacion.jpeg';
 import './styles/Home.css';
 
@@ -13,22 +13,26 @@ const words = [
 ];
 
 function Home() {
-    const [currindex, setCurrIndex] = useState(0);
-    const {lightTheme} = useContext(ThemeContext);
+    const [prevIndex, setPrevIndex] = useState(words.length - 1);
+    const [currIndex, setCurrIndex] = useState(0);
+    const [nextIndex, setNextIndex] = useState(1);
+    const { lightTheme } = useContext(ThemeContext);
     const navigate = useNavigate();
 
     useEffect(() => {
-        const rotateCurr = () => {
-            setCurrIndex(prevIndex => (prevIndex + 1) % words.length);
+        const rotate = () => {
+            setCurrIndex(index => (index + 1) % words.length);
+            setPrevIndex(index => (index + 1) % words.length);
+            setNextIndex(index => (index + 1) % words.length);
         }
-        const interval = setInterval(rotateCurr, 4000);
+        const interval = setInterval(rotate, 3000);
         return () => clearInterval(interval);
     }, [])
 
     const navigateAbout = () => {
         navigate('/about')
     }
-    
+
     const navigateProjects = () => {
         navigate('/projects')
     }
@@ -46,7 +50,7 @@ function Home() {
                         >
                             <div className='home-card card'>
                                 <div className='card-head'>
-                                    <img src={MyImg} alt='me' style={{height: '100%', width: '100%', objectFit: 'cover'}}/>
+                                    <img src={MyImg} alt='me' style={{ height: '100%', width: '100%', objectFit: 'cover' }} />
                                 </div>
                                 <div className='description card-body'>
                                     <h4 className='text-secondary'>Frontend developer</h4>
@@ -54,7 +58,7 @@ function Home() {
                                         Invested in the continuous development of my coding and people skills, I am a frontend developer
                                         with a commitment to generate impact wherever I go. Having a background in recruiting, I
                                         strive thanks to my ability to communicate and understand the needs of my colleagues and clients,
-                                        and I seek to apply this knowledge wherever my journey takes me. 
+                                        and I seek to apply this knowledge wherever my journey takes me.
                                     </p>
                                 </div>
                                 <div className='buttons card-footer'>
@@ -69,16 +73,16 @@ function Home() {
                             <h1>Ronan Filloy</h1>
                             <div className='fading-adj'>
                                 <span className='prev'>
-                                    {words[(currindex - 1 + words.length) % words.length]}
+                                    {words[prevIndex]}
                                 </span>
                                 <i className="fas fa-chevron-up"></i>
                                 <span className='curr'>
-                                    {words[currindex]}
-                                    
+                                    {words[currIndex]}
+
                                 </span>
                                 <i className="fas fa-chevron-up"></i>
                                 <span className='next'>
-                                    {words[(currindex + 1) % words.length]}
+                                    {words[nextIndex]}
                                 </span>
                             </div>
                         </div>
