a55b9b90e454d59ecf3156438dd44cb53724b82f
ronanfilloy <ronanfilloy@gmail.com>
2023-08-22 18:00:28 -0300

    added project

--- src/helpers/projectsHelpers.js
+++ src/helpers/projectsHelpers.js
@@ -2,12 +2,14 @@ import dadIcon from '../imgs/dad-jokes.png';
 import colorIcon from '../imgs/Color-picker.png';
 import loginIcon from '../imgs/Login-page.png';
 import todoIcon from '../imgs/To-do-list.png';
+import yelpIcon from '../imgs/Yelp-Camp.png';
 
 const projectsIcons = [
     { source: dadIcon, name: 'dad jokes', pageUrl: 'https://ronanfilloy.github.io/dad-jokes.github.io', codeUrl: 'https://github.com/RonanFilloy/dad-jokes.github.io' },
     { source: colorIcon, name: 'color picker', pageUrl: 'https://ronanfilloy.github.io/Color-picker.github.io', codeUrl: 'https://github.com/RonanFilloy/Color-picker.github.io'},
     { source: loginIcon, name: 'login page', pageUrl: 'https://ronanfilloy.github.io/Login-page.github.io', codeUrl: 'https://github.com/RonanFilloy/Login-page.github.io'},
-    { source: todoIcon, name: 'todo list', pageUrl: 'https://ronanfilloy.github.io/Todo-list.github.io', codeUrl: 'https://github.com/RonanFilloy/Todo-list.github.io'}
+    { source: todoIcon, name: 'todo list', pageUrl: 'https://ronanfilloy.github.io/Todo-list.github.io', codeUrl: 'https://github.com/RonanFilloy/Todo-list.github.io'},
+    { source: yelpIcon, name: 'yelp camp', pageUrl: 'https://yelpcamp-bnnd.onrender.com/', codeUrl: 'https://github.com/RonanFilloy/YelpCamp'}
 ];
 
 export default projectsIcons
