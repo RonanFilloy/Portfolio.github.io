ded4ca96ff0dd7d780d4f012cc4975beae7b82a1
ronanfilloy <ronanfilloy@gmail.com>
2023-08-30 14:56:54 -0300

    removed a project that needs tweaking

--- src/helpers/projectsHelpers.js
+++ src/helpers/projectsHelpers.js
@@ -1,4 +1,3 @@
-import dadIcon from '../imgs/dad-jokes.png';
 import colorIcon from '../imgs/Color-picker.png';
 import loginIcon from '../imgs/Login-page.png';
 import todoIcon from '../imgs/To-do-list.png';
@@ -6,7 +5,6 @@ import yelpIcon from '../imgs/Yelp-Camp.png';
 import keepIcon from '../imgs/keep-clone.png';
 
 const projectsIcons = [
-    { source: dadIcon, name: 'dad jokes', pageUrl: 'https://ronanfilloy.github.io/dad-jokes.github.io', codeUrl: 'https://github.com/RonanFilloy/dad-jokes.github.io' },
     { source: colorIcon, name: 'color picker', pageUrl: 'https://ronanfilloy.github.io/Color-picker.github.io', codeUrl: 'https://github.com/RonanFilloy/Color-picker.github.io'},
     { source: loginIcon, name: 'login page', pageUrl: 'https://ronanfilloy.github.io/Login-page.github.io', codeUrl: 'https://github.com/RonanFilloy/Login-page.github.io'},
     { source: todoIcon, name: 'todo list', pageUrl: 'https://ronanfilloy.github.io/Todo-list.github.io', codeUrl: 'https://github.com/RonanFilloy/Todo-list.github.io'},
